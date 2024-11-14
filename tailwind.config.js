db9bc4143244bc3ec008813bd2a73a09d7b50258
Nicoll0410 <nicollgiraldo0410@gmail.com>
2024-11-14 15:00:26 -0500

    se agrega contenido de la pagina de registro

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
     extend: {
       colors: {
         primary : "#ffb5de",
-        secundary : "#1E293B",
+        secundary : "#FFFFFF",
         accent: "#ff008b"
       }
     },
