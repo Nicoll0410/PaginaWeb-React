3d76018d84e02c97e9b133c4491003c2d697f61d
Nicoll0410 <nicollgiraldo0410@gmail.com>
2024-11-15 10:06:42 -0500

    se modifica el nombre del boton en la pagina de cursos

--- src/pages/Cursos.jsx
+++ src/pages/Cursos.jsx
@@ -4,7 +4,7 @@ export const Cursos = () => {
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Estos son algunos cursos que tenemos</h1>
                         <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
                     </div>
                     <div className="flex flex-wrap -m-4">
@@ -78,7 +78,7 @@ export const Cursos = () => {
                             </div>
                         </div>
                     </div>
-                    <button className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">Button</button>
+                    <button className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">Explorar Mas</button>
                 </div>
             </section>
         </>
